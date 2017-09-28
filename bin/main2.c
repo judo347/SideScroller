f3b896fdb2ae1fa42de0f87b1aeea9ee9f349deb
Mikkel K <32062449+judo347@users.noreply.github.com>
2017-09-28 20:15:39 +0200

    Update main2.c

--- bin/main2.c
+++ bin/main2.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <windows.h>
 
 #define holeChance 3
+
 
 int rng(int possible)
 {
@@ -11,6 +13,7 @@ int rng(int possible)
 
 int map(int playPosX, int playPosY, int gameState) /*OUTPUT on screen*/
 {
+    clearScr(); /* Clears the screen! */
     int i, x;
     int wide = 20;
     int high = 6;
@@ -59,12 +62,13 @@ int map(int playPosX, int playPosY, int gameState) /*OUTPUT on screen*/
         else
             printf(" ");
     }
+
     return 1;
 }
 
 int jump() /* PLAYER JUMPING */
 {
-
+    return 0;
 }
 
 void clearScr() /* CLEARS THE SCREEN */
@@ -72,23 +76,33 @@ void clearScr() /* CLEARS THE SCREEN */
     system("@cls||clear");
 }
 
-int move(int playPosX, int playPosY) /* HANDLING PLAYER MOVEMENT/INPUT int playPosX, int playPosY*/
+void move(int playPosX, int playPosY, int* poiX, int* poiY) /* HANDLING PLAYER MOVEMENT/INPUT int playPosX, int playPosY*/
 {
-    int playPos[2] = {playPosX,playPosY};
+    /* int playPos[2] = {playPosX,playPosY}; */
     int c = getche();
     switch(c)
     {
-        case 'w' : printf("w\n");
+        case 'w' : printf("w\n"); /* JUMP */
+                   playPosX = playPosX + 1;
+                   playPosY = playPosY - 1;
+                   map(playPosX, playPosY, 1);
+                   Sleep(500);
+                   playPosX = playPosX + 1;
+                   map(playPosX, playPosY, 1);
+                   Sleep(500);
+                   *poiX = playPosX + 1;
+                   *poiY = playPosY + 1;
+
             break;
-        case 'a' : playPos[0]--; /* x- */
+        case 'a' : *poiX = --playPosX; /* x--  playPos[0]--*/
             break;
         case 's' : printf("s\n");
             break;
-        case 'd' : printf("d\n");
+        case 'd' : *poiX = ++playPosX; /* x++ */
             break;
         default : printf("Something went wrong!\n");
     }
-    return playPos;
+    /* return playPos; */
 }
 
 
@@ -98,19 +112,22 @@ int main(void)
     int gameState = 1; /* 1 = running, 0 = game has ended */
     srand(time(NULL));
     int i, x;
-    int playX = 5, playY = 5;
-    int playPos[2] = {playX, playY};
+    int playPosX = 5, playPosY = 5, poiX = 5, poiY = 5;
+    /* int playPos[2] = {playX, playY}; */
 
     /* int playerPosX = 5, playerPosY = 5; */
 
     while(gameState)
     {
-        //map(playerPosX,playerPosY,gameState);
-        //printf("%d %d", playerPosX, playerPosY);
-        map(playPos[0],playPos[1],gameState);
-        printf("%d %d", playPos[0], playPos[1]);
-        playPos = move(playPos);
-        clearScr();
+        /* printf("%d %d", playPosX, playPosY); /* TEST */
+        map(playPosX, playPosY, gameState);
+        printf("%d %d", playPosX, playPosY); /* TEST */
+
+        move(playPosX, playPosY, &poiX, &poiY); /* Sending the x and y coord to move-function (and the pointers) */
+        playPosX = poiX; /* Saving the pointer in the variable */
+        playPosY = poiY; /* Saving the pointer in the variable  */
+
+
 
     }
     /*
